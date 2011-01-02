70eff40ee1a870cf44b873bff2a468f1a33ccb7f
Matt Fletcher <fletcher@atomicobject.com>
2011-01-02 09:22:30 -0500

    use both stdlib and rspec matchers (for raise_error)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 require_relative "../lib/minilab"
 
 RSpec.configure do |config|
-  config.expect_with :stdlib
   config.mock_framework = :rr
+
+  # Using both to minimize the amount of porting work.
+  # most assertions are still test:unit based, but some
+  # (like exceptions) us rspec
+  config.expect_with :stdlib, :rspec
 end
